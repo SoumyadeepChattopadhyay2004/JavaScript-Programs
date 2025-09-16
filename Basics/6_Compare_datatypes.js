2a5e725e44fdf7e1c96d2090247be4eac0e1fae4
Soumyadeep Chattopadhyay <154803056+SoumyadeepChattopadhyay2004@users.noreply.github.com>
2025-09-16 20:20:46 +0000

    My JavaScript Programs

---
+++ Basics/6_Compare_datatypes.js
@@ -0,0 +1,20 @@
+console.log(1 < 3)
+console.log(3 > 1)
+console.log(3 >= 3)
+console.log(3 <= 3)
+console.log(3 == 3)
+console.log(3 != 3)
+
+console.log("3" > 1) //true
+console.log("03" > 1) //true
+
+console.log(null > 0) //false
+console.log(null == 0) //false
+console.log(null >= 0) //true
+
+console.log(undefined == 0) //false
+console.log(undefined > 0) //false
+console.log(undefined < 0) //false
+
+//===
+console.log("3" === 3) ;//false
