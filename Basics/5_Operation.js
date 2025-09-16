bda11fe5ebe93857a01e69d63b1b6cb5ce3d9037
Soumyadeep Chattopadhyay <154803056+SoumyadeepChattopadhyay2004@users.noreply.github.com>
2025-09-16 20:12:04 +0000

    My JavaScript Programs

---
+++ Basics/5_Operation.js
@@ -0,0 +1,39 @@
+//**************Operations**************//
+
+let val = 3
+let negVal = -val
+//console.log(negVal)
+
+//console.log(4+4);
+//console.log(4-4);
+//console.log(4*4);
+//console.log(4**4)
+//console.log(4/4);
+//console.log(4%4);
+
+/*
+let str1 = "hello"
+let str2 = " Soumyadeep"
+let str3 = str1+str2
+console.log(str3)
+*/
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(true)
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+3
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
