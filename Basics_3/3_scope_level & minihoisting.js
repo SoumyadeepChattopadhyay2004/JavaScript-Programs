dce7f90c7022619deb9a4f2d3c1477800152f1f9
Soumyadeep Chattopadhyay <154803056+SoumyadeepChattopadhyay2004@users.noreply.github.com>
2025-09-18 16:10:49 +0000

    My JavaScript Programs

--- Basics_3/3_scope_level & minihoisting.js
+++ Basics_3/3_scope_level & minihoisting.js
@@ -1,11 +1,32 @@
-// ne
+/*
+// nested functions
 function one(){
   const username = "Soumya";
   function two(){
     const website = "youtube.com/soumya"; 
     console.log(username); // Soumya // inner function can access variables of outer function
   }
-  two(); 
   console.log(website); 
+  two(); 
 }
-one();
+// one();
+
+if (true){
+  const username = "Soumya"
+  if (username === "Soumya"){
+    const website = "youtube"
+    console.log(username+website);
+  }
+  console.log(website); // can't access
+}
+*/
+//----------------interesting--------------------------//
+
+console.log(addone(5))
+function addone(num) {
+  return num + 1 // 6
+}
+addtwo(5)
+const addtwo = function(num) {
+  return num + 2 // it shows error as value of function is stored in variable
+}
